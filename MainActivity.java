46404c68d5899e6cb647db3e124bbd52b9dc86da
gerald krug <gdkss@hotmail.com>
2025-12-29 13:40:56 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.keepclone;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(android.R.layout.simple_list_item_1);
     }
 }
