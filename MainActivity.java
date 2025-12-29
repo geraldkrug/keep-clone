fe048573bff3c56263362d57ee666c360d728d27
gerald krug <gdkss@hotmail.com>
2025-12-29 12:49:20 -0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,11 @@
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.simple_list_item_1);
+    }
+}
